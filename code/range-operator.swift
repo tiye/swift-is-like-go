aba68d524d1bf6230124d85813d05873a1c7c84d
Pascal Batty <pascal.batty@soat.fr>
2016-04-08 11:45:25 +0200

    Replaced the range operator excluding the upper boundary

--- code/range-operator.swift
+++ code/range-operator.swift
@@ -1,6 +1,6 @@
 let names = ["Anna", "Alex", "Brian", "Jack"]
 let count = names.count
-for i in 0..count {
+for i in 0..<count {
     print("Person \(i + 1) is called \(names[i])")
 }
 // Person 1 is called Anna
