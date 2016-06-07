e2e85b265ab5497207a097ad910f8ee3b51f2109
Gautier Nilhcem <nilhcem@gmail.com>
2016-06-07 15:04:21 +0200

    Fix invalid kotlin comment

--- code/named-arguments.kt
+++ code/named-arguments.kt
@@ -1,6 +1,6 @@
 fun area(width: Int, height: Int) = width * height
 area(width = 2, height = 3)
 
-# This is also possible with named arguments
+// This is also possible with named arguments
 area(2, height = 2)
 area(height = 3, width = 2)
