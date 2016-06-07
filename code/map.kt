681ba683e543a15ece7308b9b4566051e9aa2089
Gautier Nilhcem <nilhcem@gmail.com>
2016-06-07 11:32:58 +0200

    Improve map sample

--- code/map.kt
+++ code/map.kt
@@ -1,2 +1,2 @@
-var numbers = listOf(20, 19, 7, 12)
+val numbers = listOf(20, 19, 7, 12)
 numbers.map { 3 * it }
--- code/map.swift
+++ code/map.swift
@@ -1,2 +1,2 @@
 let numbers = [20, 19, 7, 12]
-numbers.map { 3 *  $0 }
+numbers.map { 3 * $0 }
